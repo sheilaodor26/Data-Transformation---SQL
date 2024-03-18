52e556d179e79a5d0f9c70c7dc7f021d8b709c0e
sheilaodor26 <163795763+sheilaodor26@users.noreply.github.com>
2024-03-18 01:23:53 -0400

    Update New_Column.sql

--- New_Column.sql
+++ New_Column.sql
@@ -1,1 +1,12 @@
-
+```
+SELECT 
+    Name,
+    Platform,
+    Global_Sales,
+    CASE
+        WHEN Year < 2010 THEN 'pre-2010'
+        ELSE 'post-2010'
+    END AS year_category
+FROM
+    vgsales;
+```
