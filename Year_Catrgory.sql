ccf4bf47bbb56cc3e6361f8be9e0b7ed2f149e7c
sheilaodor26 <163795763+sheilaodor26@users.noreply.github.com>
2024-03-18 01:16:55 -0400

    Update Year_Catrgory.sql

--- Year_Catrgory.sql
+++ Year_Catrgory.sql
@@ -1,1 +1,12 @@
-
+```
+SELECT 
+    CASE
+        WHEN year < 2010 THEN 'pre-2010'
+        ELSE 'post-2010'
+    END AS year_category,
+    AVG(global_sales) AS avg_global_sales
+FROM
+    vgsales
+GROUP BY year_category
+ORDER BY avg_global_sales DESC;
+```
